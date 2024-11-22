a64c59613a841dadada8bfd45c32e9f0229a3ab0
newtoncp2 <jeanmicheljoliveira@gmail.com>
2024-11-22 12:02:30 -0300

    adicao da classe disciplinas

--- app/models.py
+++ app/models.py
@@ -3,14 +3,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 
+class Discipline(models.Model):
+  code = models.CharField(max_length=10)
+  name = models.CharField(max_length = 100)
+  image = models.FileField()
+
 class Course(models.Model):
   name = models.CharField(max_length=40)
   dificulty = models.IntegerField(default=0)
-  # Adicionar imagem
   # A relação com a prova já está sendo descrito no ManyToMany do Exam
-
+  discipline = models.ForeignKey(Discipline, on_delete = models.CASCADE)
   def __str__(self):
     return f"{self.name}"
+  
+
 
 
 class UserCourse(models.Model):
