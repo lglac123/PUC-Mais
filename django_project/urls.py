2eb53001972798efa7d62a97e7d56e0b8bdf9c27
joaocapechi <joaopedrocapechi@gmail.com>
2024-11-26 08:24:13 -0300

    Corrigindo a userCourse

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # path("aulas/", views.aulas),
     path('perfil/', views.perfil),
     path('disciplinas/', views.disciplinas),
-    path("disciplinas/<str:course_name>", views.Disciplina),
+    path("disciplinas/<str:course_name>", views.Disciplina, name="Disciplina"),
     path("disciplinas/<str:course_name>/aulas&listas/", views.aulas_listas_basic),
     path("disciplinas/<str:course_name>/provas/", views.provas),
     path("registerCourse/", views.userCourse),
